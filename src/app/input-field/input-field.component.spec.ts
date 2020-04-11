574643ddd46181f0a328fdbd6844b1bdcfb45f63
nikolajgriskin <din.nick@bk.ru>
2020-04-11 21:54:06 +0500

    component value tests

--- src/app/input-field/input-field.component.spec.ts
+++ src/app/input-field/input-field.component.spec.ts
@@ -1,6 +1,16 @@
+import { By } from '@angular/platform-browser';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { filter, take } from 'rxjs/operators';
 
 import { InputFieldComponent } from './input-field.component';
+
+const setInputValue = (fixture: ComponentFixture<any>, selector: string, value: string) => {
+  const input = fixture.debugElement.query(By.css(selector));
+  const el = input.nativeElement;
+
+  el.value = value;
+  el.dispatchEvent(new Event('input'));
+};
 
 describe('InputFieldComponent', () => {
   let component: InputFieldComponent;
@@ -21,5 +31,20 @@ describe('InputFieldComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('simple input test', () => {
+    setInputValue(fixture, 'input', 'someValue');
+    return component.currentValue.pipe(filter(val => val !== null), take(1)).toPromise()
+      .then(value => expect(value).toBe('someValue'));
+  });
+
+  it('simple input test with many changes of value', () => {
+    setInputValue(fixture, 'input', 'someValue 1');
+    setInputValue(fixture, 'input', 'someValue 2');
+    setInputValue(fixture, 'input', 'someValue 3');
+
+    return component.currentValue.pipe(filter(val => val !== null), take(1)).toPromise()
+      .then(value => expect(value).toBe('someValue 3'));
   });
 });
